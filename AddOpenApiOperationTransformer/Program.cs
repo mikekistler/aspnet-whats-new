d0cd7d8cf506142b67d574b99648c5094cbec06c
Mike Kistler <mikekistler@microsoft.com>
2025-04-21 14:01:01 -0500

    WIP

--- AddOpenApiOperationTransformer/Program.cs
+++ AddOpenApiOperationTransformer/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -31,11 +34,72 @@ app.MapGet("/weatherforecast", () =>
         .ToArray();
     return forecast;
 })
-.WithName("GetWeatherForecast");
+.WithName("GetWeatherForecast")
+.AddOpenApiOperationTransformer((operation, context, token) =>
+{
+    operation.Responses["200"].Description = "A custom description for the 200 response.";
+    operation.Responses["200"].Headers["X-Custom-Header"] = new OpenApiHeader
+    {
+        Description = "A custom header for the 200 response.",
+        Required = false,
+        Schema = new OpenApiSchema
+        {
+            Type = JsonSchemaType.String
+        }
+    };
+    return Task.CompletedTask;
+});
+
+app.MapPost("/weatherforecast", (WeatherForecast forecast) =>
+{
+    return TypedResults.Created($"/weatherforecast/{forecast.Date}", forecast);
+})
+.WithName("CreateWeatherForecast")
+.WithResponseDescription(201, "The forecast was created successfully.")
+.WithResponseDescription(418, "I'm a teapot.")
+.WithLocationHeader();
 
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+// Static class for operation transformers
+public static class ExtensionMethods
+{
+    public static RouteHandlerBuilder WithResponseDescription(this RouteHandlerBuilder builder, int statusCode, string description)
+    {
+        builder.AddOpenApiOperationTransformer((operation, context, cancellationToken) =>
+        {
+            // operation.Responses?.TryGetValue(statusCode.ToString(), out var response);
+            var response = operation.Responses?.TryGetValue(statusCode.ToString(), out var r) == true ? r : null;
+            // The following line uses the new "null conditional assignment" feature of C# 14
+            response?.Description = description;
+            return Task.CompletedTask;
+        });
+        return builder;
+    }
+
+    public static RouteHandlerBuilder WithLocationHeader(this RouteHandlerBuilder builder)
+    {
+        builder.AddOpenApiOperationTransformer((operation, context, cancellationToken) =>
+        {
+            var createdResponse = operation?.Responses?.TryGetValue("201", out var r) == true ? r : null;
+            // The following line uses the new "null conditional assignment" feature of C# 14
+            createdResponse?.Headers["Location"] = new OpenApiHeader
+            {
+                Description = "Location of the created resource.",
+                Required = true,
+                Schema = new OpenApiSchema
+                {
+                    Type = JsonSchemaType.String,
+                    Format = "uri"
+                }
+            };
+            return Task.CompletedTask;
+        });
+        return builder;
+    }
 }
