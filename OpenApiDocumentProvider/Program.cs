5e0ee0795de6faaedc9a1c2b9bac48757ae2d0b5
Mike Kistler <mikekistler@microsoft.com>
2025-05-03 13:16:58 -0500

    WIP

--- OpenApiDocumentProvider/Program.cs
+++ OpenApiDocumentProvider/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi.Writers;
+
+const string documentName = "v1";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(documentName);
 
 var app = builder.Build();
 
@@ -32,6 +38,27 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 })
 .WithName("GetWeatherForecast");
+
+if (args.Any(arg => arg.Equals("--generate-openapi")))
+{
+    var documentProvider = app.Services.GetRequiredKeyedService<IOpenApiDocumentProvider>(documentName);
+
+    var document = await documentProvider.GetOpenApiDocumentAsync();
+
+    // Serialize the OpenAPI document to disk.
+    var path = $"{documentName}.json";
+    using var fileStream = new FileStream(path, FileMode.Create);
+    using var writer = new StreamWriter(fileStream);
+    var jsonWriter = new OpenApiJsonWriter(writer);
+    await document.SerializeAsync(jsonWriter, Microsoft.OpenApi.OpenApiSpecVersion.OpenApi3_1);
+    await writer.FlushAsync();
+
+    writer.Dispose();
+    fileStream.Dispose();
+
+    Console.WriteLine($"OpenAPI document generated at {path}");
+    return;
+}
 
 app.Run();
 
